b87bbea96303beee215ebcf666a49a9bbabbc340
mcklab <mcklab@libwk6f7m214361.local>
2013-01-09 09:44:41 -0500

    Add all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dhwi::Application.config.secret_token = '29d10d7bae52cbfaf9dcd4e3e5fd8fe716d4c3cbe87049e714433fa901163f84d5c8a51570995a5f5f83aa79b675d713568fc38c686f76d52134668b05510a94'
